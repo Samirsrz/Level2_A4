64bd7db629b632942287d7edded643f26611424e
Samir Uddin Ahamed <shahilahamed2001@gmail.com>
2026-07-11 17:07:08 +0600

    added global error handler and notfound

--- src/modules/rentalRequest/rentalRequest.route.ts
+++ src/modules/rentalRequest/rentalRequest.route.ts
@@ -11,9 +11,7 @@ router.post('/rentals',auth("TENANT"),rentalController.createRentalRequest)
 router.get('/rentals',auth("TENANT"),rentalController.getMyrentalRequest)
 
 router.get('/rentals/:id',auth("TENANT","LANDLORD"),rentalController.getRentalRequestById)
-
 router.get('/landlord/requests',auth("LANDLORD"),rentalController.getLandlordPropertyRequests)
-
 router.patch('/landlord/requests/:id',auth("LANDLORD"),rentalController.updateStatusOfRequest)
 
 
